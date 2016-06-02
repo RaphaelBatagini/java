4b3bf5e6670063ec9d1731f205302b4e7da9550f
raphael <raphael@progiro.com.br>
2016-06-02 17:09:51 -0300

    added comment

--- MyApplication/MyApplication.java
+++ MyApplication/MyApplication.java
@@ -1,3 +1,6 @@
+/* No matter the name of the file, when you compile a .class file 
+ * will be generated with the name of the class. In this case MyApplication.class
+ */
 class MyApplication{
 	public static void main(String[] args){
 		System.out.println("Minha aplicacao");
