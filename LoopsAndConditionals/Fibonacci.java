4ac41de5ea0d16b00eb45b013b4e5804de5a0369
raphael <raphael@progiro.com.br>
2016-06-06 15:32:43 -0300

    fibonacci

--- LoopsAndConditionals/Fibonacci.java
+++ LoopsAndConditionals/Fibonacci.java
@@ -1,5 +1,19 @@
 class Fibonacci{
 	public static void main(String[] args){
-		
+		int number = 1;
+		int previousNumber = 0;
+		int n2;
+
+		do {
+			if (previousNumber == 0) {
+				System.out.println(previousNumber);
+				System.out.println(number);
+				previousNumber++;
+			} else {
+				number += previousNumber;
+				System.out.println(number);
+				previousNumber = number;
+			}
+		} while(number < 100);
 	}
 }
