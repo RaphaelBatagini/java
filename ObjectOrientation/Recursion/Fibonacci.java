3f93ec6fff409ab6caaef0db7451253480ce3d66
Raphael den Hartog Batagini <raphael@progiro.com.br>
2016-06-13 15:41:16 -0300

    recursive fibonacci

--- ObjectOrientation/Recursion/Fibonacci.java
+++ ObjectOrientation/Recursion/Fibonacci.java
@@ -1,5 +1,5 @@
 class Fibonacci{
 	int calculateFibonacci(int num){
-				
+		return (num == 0) ? 0 : (num == 1) ? 1 : calculateFibonacci(--num) + calculateFibonacci(--num);
 	}	
 }
--- ObjectOrientation/Recursion/Test.java
+++ ObjectOrientation/Recursion/Test.java
@@ -1,7 +1,7 @@
 class Test {
 	public static void main(String[] args){
 		Fibonacci fibonacci = new Fibonacci();
-		for (int i = 1; i <= 6; i++) {
+		for (int i = 0; i <= 6; i++) {
 			int result = fibonacci.calculateFibonacci(i);
   			System.out.println(result);
 		}
