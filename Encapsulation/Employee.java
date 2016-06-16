219bfd4463c90a3b3dfd2516c9bc8ef762835340
Raphael Batagini <raphael@progiro.com.br>
2016-06-15 23:06:18 -0300

    encapsulation

---
+++ Encapsulation/Employee.java
@@ -0,0 +1,5 @@
+class Employee{
+	private String name = "Bill Sample";
+	private int age = 23;
+	private float salary = 1000.00;
+}
---
+++ Encapsulation/Test.java
@@ -0,0 +1,6 @@
+class Test {
+	public static void main(String[] args) {
+		Employee employee = new Employee();
+		employee.salary = 5000.00;
+	}
+}
