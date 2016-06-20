a06b2de42448916e16f5e9ba1c587fa4ac170860
Raphael Batagini <raphael@progiro.com.br>
2016-06-20 19:03:24 -0300

    encapsulation

--- ObjectOrientation/Encapsulation/Employee.java
+++ ObjectOrientation/Encapsulation/Employee.java
@@ -1,5 +1,29 @@
 class Employee{
 	private String name = "Bill Sample";
 	private int age = 23;
-	private float salary = 1000.00f;
+	private double salary = 1000.00f;
+
+	public Employee(){
+		//default constructor with no arguments
+	}
+
+	public Employee(String name){
+		this.name = name;
+	}
+
+	public double getSalary(){
+		return this.salary;
+	}
+
+	public void setSalary(double salary){
+		this.salary = salary;
+	}
+
+	public double getAnualEarnings(){
+		return this.salary * 12;
+	}
+
+	public String getName(){
+		return this.name;
+	}
 }
--- ObjectOrientation/Encapsulation/TestError.java
+++ ObjectOrientation/Encapsulation/TestError.java
@@ -2,6 +2,6 @@ class TestError {
 	public static void main(String[] args) {
 		Employee employee = new Employee();
 		//throw error bacause try access a private attribute
-		employee.salary = 5000.00f;
+		employee.salary = 5000.00;
 	}
 }
